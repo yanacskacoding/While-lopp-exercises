049110ed01534b8a59713738e8e273938208fd87
yanacskacoding <78523922+yanacskacoding@users.noreply.github.com>
2022-11-23 18:00:49 +0700

    Update while_loop.py

--- while_loop.py
+++ while_loop.py
@@ -7,7 +7,7 @@ There are 12 numbers divisible by 5 and 7.
 5,7,10,14,15,20,21,25,28,30,35,40,       '''
 
 #code:
-'''
+
 def divisible(num):
     if int(num):
         if (int(num)/5).is_integer() or (int(num)/7).is_integer():
@@ -27,7 +27,7 @@ while thisNum != int(num)+1:
 print(numbers)
 print("There are", len(numbers), "numbers divisible by 5 or 7")
 
-'''
+
 
 '''Write a program that prints out the multiplication table of the entered number by the user by using whileloop.
 
@@ -47,7 +47,7 @@ Example 1:
 
 '''
 #code:
-'''
+
 num = int(input())
 
 i = 1
@@ -56,7 +56,7 @@ while i <= 10:
     print(num, "x", i, "=", num*i)
     i += 1
 
-'''
+
 
 '''Write a program that asks a user to enter a number and print out the sum of the powers of 2 from 0 to N using while loop.
 
